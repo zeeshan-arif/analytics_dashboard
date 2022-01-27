c02b22df27acfe7f3de69b586687e0a85c6e8fef
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-01-27 12:36:07 +0530

    merged the product and purchase dataframes and displayed on templates

--- products/views.py
+++ products/views.py
@@ -11,8 +11,11 @@ from .models import Product, Purchase
 def chart_select_view(request):
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
+    product_df['product_id'] = product_df['id']
+    df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x':'id', 'date_x':'date'}, axis=1)
     context = {
         'products': product_df.to_html(),
         'purchase': purchase_df.to_html(),
+        'df': df.to_html(),
     }
     return render(request, 'products/main.html', context)
