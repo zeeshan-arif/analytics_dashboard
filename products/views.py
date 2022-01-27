2fccb478101777427fe3f5c3b9bb3f09ec73a6de
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-01-27 14:43:58 +0530

    modified products-views file and products-main.html file

--- products/views.py
+++ products/views.py
@@ -1,4 +1,3 @@
-from multiprocessing import context
 from django.shortcuts import render
 
 import pandas as pd
@@ -9,13 +8,23 @@ from .models import Product, Purchase
 
 
 def chart_select_view(request):
+    error_message = None
+    df = None
+
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
     product_df['product_id'] = product_df['id']
-    df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x':'id', 'date_x':'date'}, axis=1)
+
+    if purchase_df.shape[0] > 0:
+        df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x':'id', 'date_x':'date'}, axis=1)
+        if request.method == "POST":
+            chart_type = request.POST.get('sales')
+            date_from = request.POST.get('date_from')
+            date_to = request.POST.get('date_to')
+    else:
+        error_message = "No records in the database"
+
     context = {
-        'products': product_df.to_html(),
-        'purchase': purchase_df.to_html(),
-        'df': df.to_html(),
+        'error_message': error_message,
     }
     return render(request, 'products/main.html', context)
