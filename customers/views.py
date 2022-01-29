2624c5e381e2a7539ff7cd7ae7eb27868bb07ca0
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-01-30 00:26:57 +0530

    created customer_corr_view connected it with urls.py file and template to show graph

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from .models import Customer
+from products.utils import get_image
 
 # Create your views here.
+
+def customer_corr_view(request):
+    df = pd.DataFrame(Customer.objects.all().values)
+    corr = round(df['budget'].corr(df['employment']), 2)
+
+    plt.switch_backend('Agg')
+    sns.jointplot(x='budget', y='employment', kind='reg', data=df).set_axis_labels('Company budget', 'Number of employees')
+    graph = get_image()
+
+    context = {
+        'graph': graph,
+        'corr': corr,
+    }
+
+    return render(request, 'customers/main.html', context)
