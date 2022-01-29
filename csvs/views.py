562b68033f3673472b3e398d6755cc699334abec
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-01-29 14:16:01 +0530

    deleted all migration files and database, again re-migrated the whole project and added additional conditions if no data is present to show message

--- csvs/views.py
+++ csvs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import CsvForm
 
 # Create your views here.
 
--- products/views.py
+++ products/views.py
@@ -15,29 +15,34 @@ def chart_select_view(request):
     df = None
     price = None
 
-    product_df = pd.DataFrame(Product.objects.all().values())
-    purchase_df = pd.DataFrame(Purchase.objects.all().values())
-    product_df['product_id'] = product_df['id']
+    try:
+        product_df = pd.DataFrame(Product.objects.all().values())
+        purchase_df = pd.DataFrame(Purchase.objects.all().values())
+        product_df['product_id'] = product_df['id']
+    except:
+        product_df = None
+        purchase_df = None
 
-    if purchase_df.shape[0] > 0:
-        df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x':'id', 'date_x':'date'}, axis=1)
-        price = df['price']
-        if request.method == "POST":
-            chart_type = request.POST.get('sales')
-            date_from = request.POST.get('date_from')
-            date_to = request.POST.get('date_to')
+    if purchase_df:
+        if purchase_df.shape[0] > 0:
+            df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x':'id', 'date_x':'date'}, axis=1)
+            price = df['price']
+            if request.method == "POST":
+                chart_type = request.POST.get('sales')
+                date_from = request.POST.get('date_from')
+                date_to = request.POST.get('date_to')
 
-            df['date'] = df['date'].apply(lambda x: x.strftime("%Y-%m-%d"))
-            df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
+                df['date'] = df['date'].apply(lambda x: x.strftime("%Y-%m-%d"))
+                df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
 
-            if chart_type != "":
-                if date_from != "" and date_to != "":
-                    df = [(df['date'] > date_from) & (df['date'] < date_to)]
-                    df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
-                # function to get a graph
-                graph = get_simple_plot(chart_type, x=df2['date'], y=df2['total_price'], data=df)
-            else:
-                error_message = 'Please select a chart type to continue'
+                if chart_type != "":
+                    if date_from != "" and date_to != "":
+                        df = [(df['date'] > date_from) & (df['date'] < date_to)]
+                        df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
+                    # function to get a graph
+                    graph = get_simple_plot(chart_type, x=df2['date'], y=df2['total_price'], data=df)
+                else:
+                    error_message = 'Please select a chart type to continue'
     else:
         error_message = "No records in the database"
 
