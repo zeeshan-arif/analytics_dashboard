773f3c4103bd117ad646c811f5de473bc370624c
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-01-29 16:11:54 +0530

    successfully able to upload the csv format file and verfied by looking at admin

--- csvs/views.py
+++ csvs/views.py
@@ -4,4 +4,12 @@ from .forms import CsvForm
 # Create your views here.
 
 def upload_file_view(request):
-    return render(request, 'csvs/upload.html', {})
+    form = CsvForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        form = CsvForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'csvs/upload.html', context)
