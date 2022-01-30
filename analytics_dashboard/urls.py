cabfab15ad765101fc5cee725a427a53008779db
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-01-31 00:46:39 +0530

    made changes to project urls.py file

--- analytics_dashboard/urls.py
+++ analytics_dashboard/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
     path('customers/', include('customers.urls', namespace = 'customers')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
