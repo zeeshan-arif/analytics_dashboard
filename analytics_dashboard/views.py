e83c1f5f4cfacf87c1c6556a29632fbd78343919
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-01-30 14:03:48 +0530

    created forms.py for login functionality, created login_view() and login.html template

--- analytics_dashboard/views.py
+++ analytics_dashboard/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 def home_view(request):
     return render(request, 'home.html', {})
+
+
+def login_view(request):
+    error_message = None
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(data = request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+
+    
+    context = {
+        'form': form
+    }
+    return render(request,'login.html', context)
